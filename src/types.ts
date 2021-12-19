f0b869520d58bad21aa2e81152ba8cd6281795a1
Federico Luzzi <fedeluzzi00@gmail.com>
2021-12-18 20:28:46 -0500

    chore: reintroduce worker nonce

--- src/types.ts
+++ src/types.ts
@@ -29,6 +29,7 @@ export interface JobWorkableArgs {
   workerAddress: string;
   rpcUrl: string;
   coreMessage$: Observable<CoreMessage>;
+  workerNonce: number;
   fork: GanacheFork;
   block$: Observable<Block>;
   retryId?: string;
